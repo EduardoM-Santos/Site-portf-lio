e7d7c25beffb3cff1ad429c8279c5b6a0a24bc24
Eduardo Menezes Dos Santos <156352909+EduardoM-Santos@users.noreply.github.com>
2026-05-07 12:32:39 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const botãoAbrir = document.getElementById('conteudo-sobre')
-const conteudoSobre = document.querySelector('.conteudo-oculto')
-
-botãoAbrir.addEventListener('click', () =>{
-  conteudoSobre.classList.toggle('mostrar')  
-})
-
-
